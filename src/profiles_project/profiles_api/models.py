e22d3ed7b171b040dfd89c7101514771790f783b
shlok sharma <shloksharma347@gmail.com>
2019-05-05 10:26:00 +0530

    enabled django admin

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -3,45 +3,68 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 
 class UserProfileManager(BaseUserManager):
+    """Class required by Django for managing our users from the management
+    command.
+    """
 
-    def create_user(self, email, name, password= None):
+    def create_user(self, email, name, password=None):
+        """Creates a new user with the given detials."""
+
+        # Check that the user provided an email.
         if not email:
-            raise ValueError('User must have email address')
+            raise ValueError('Users must have an email address.')
 
-        email=self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        # Create a new user object.
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
 
+        # Set the users password. We use this to create a password
+        # hash instead of storing it in clear text.
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
+
         return user
 
-    def create_superuser(self,email,username, password):
-        user= self.create_user(email,name, password)
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given detials."""
 
-        user.is_superuser=True
+        # Create a new user with the function we created above.
+        user = self.create_user(
+            email,
+            name,
+            password
+        )
+
+        # Make this user an admin.
+        user.is_superuser = True
         user.is_staff = True
+        user.save(using=self._db)
 
-        user.save(using=self.db)
         return user
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    user_id = models.CharField(max_length=64, unique=True)
+    email = models.CharField(max_length=64, unique=True)
     name = models.CharField(max_length=64)
     password = models.CharField(max_length=64)
-    imei = models.CharField(max_length=64, unique=True)
+    user_password= models.CharField(max_length=254)
+    imei = models.CharField(max_length=64)
     lock_password = models.CharField(max_length=32)
     master_id = models.CharField(max_length=64)
     master_password = models.CharField(max_length=32)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
-    USERNAME_FIELD = 'user_id'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        return self.name
+        self.name
 
     def get_short_name(self):
-        return self.name
+        self.name
 
     def __str__(self):
-        return self.user_id
+        return self.email
