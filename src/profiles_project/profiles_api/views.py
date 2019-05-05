7f1df521ad22496259f5305cad04fa9152819632
shlok sharma <shloksharma347@gmail.com>
2019-05-05 13:12:34 +0530

    added Viewsets and CRUD Methods

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -23,7 +24,7 @@ class HelloApiView(APIView):
         serializer = serializers.HelloSerializer(data=request.data)
 
         if serializer.is_valid():
-            name= serializer.data.get('name', 'imei')
+            name= serializer.data.get('name')
             imei= serializer.data.get('imei')
 
             message = 'Hello: {} and imei: {}'.format(name, imei)
@@ -40,3 +41,41 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk=None):
             return Response({'method': 'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        a_viewset = [
+        'Uses CRUD Actions',
+        'Automatically maps to urls using routers',
+        'more functionality with less code'
+        ]
+
+        return Response({'message': 'Hello', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name= serializer.data.get('name')
+            imei= serializer.data.get('imei')
+
+            message = 'Hello: {} and imei: {}'.format(name, imei)
+            return Response({'message':message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        return Response({'http_method': 'DELETE'})
