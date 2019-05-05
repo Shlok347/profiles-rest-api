ce5253c1611ef38f63cb9a5f59d12a4b8c55f5be
shlok sharma <shloksharma347@gmail.com>
2019-05-05 12:13:50 +0530

    added post and all methods

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 # Create your views here.
 class HelloApiView(APIView):
     """Test Api View"""
+
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
 
         an_apiview = [
@@ -13,3 +18,25 @@ class HelloApiView(APIView):
         'Mapped Manually To Urls',
         ]
         return Response({'message':'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name= serializer.data.get('name', 'imei')
+            imei= serializer.data.get('imei')
+
+            message = 'Hello: {} and imei: {}'.format(name, imei)
+            return Response({'message':message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+            return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+            return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+            return Response({'method': 'delete'})
